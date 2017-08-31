2190f8911cdc5e90ab9ceac44bfd1dc12a3152e4
ezio84 <brabus84@gmail.com>
2017-08-31 21:24:26 +0200

    base: Add some static custom utilities
    Add api to toggle flashlight and check if device has flashlight
    
    statusbar service getter borrowed from PhoneWindowManager code
    
    use PackageManager to check if the device has flashlight
    
    use aosp flashcontroller to handle the flash toggle like the flashlight
    tile does
    
    idoybh (YAAP): Adapted to Sv2
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I4ab5f80f4ca449220c53817aee0258429a1b6530

--- core/java/com/android/internal/statusbar/IStatusBarService.aidl
+++ core/java/com/android/internal/statusbar/IStatusBarService.aidl
@@ -235,4 +235,9 @@ interface IStatusBarService
      * Starts the default assistant app.
      */
     void startAssist(in Bundle args);
+
+    /**
+     * Custom extensions
+     */
+    void toggleCameraFlash();
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -178,6 +178,8 @@ public class CommandQueue extends IStatusBar.Stub implements
     private static final int MSG_IMMERSIVE_CHANGED = 78 << MSG_SHIFT;
     private static final int MSG_SET_QS_TILES = 79 << MSG_SHIFT;
     private static final int MSG_ENTER_DESKTOP = 80 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_CAMERA_FLASH = 83 << MSG_SHIFT;
+
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
     public static final int FLAG_EXCLUDE_RECENTS_PANEL = 1 << 1;
@@ -526,6 +528,8 @@ public class CommandQueue extends IStatusBar.Stub implements
          * @see IStatusBar#enterDesktop(int)
          */
         default void enterDesktop(int displayId) {}
+
+        default void toggleCameraFlash() { }
     }
 
     @VisibleForTesting
@@ -1435,6 +1439,16 @@ public class CommandQueue extends IStatusBar.Stub implements
         mHandler.obtainMessage(MSG_ENTER_DESKTOP, args).sendToTarget();
     }
 
+    @Override
+    public void toggleCameraFlash() {
+        synchronized (mLock) {
+            if (mHandler.hasMessages(MSG_TOGGLE_CAMERA_FLASH)) {
+                mHandler.removeMessages(MSG_TOGGLE_CAMERA_FLASH);
+            }
+            mHandler.sendEmptyMessage(MSG_TOGGLE_CAMERA_FLASH);
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -1719,6 +1733,11 @@ public class CommandQueue extends IStatusBar.Stub implements
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).setUdfpsRefreshRateCallback(
                                 (IUdfpsRefreshRateRequestCallback) msg.obj);
+                    }
+                    break;
+                case MSG_TOGGLE_CAMERA_FLASH:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).toggleCameraFlash();
                     }
                     break;
                 case MSG_SHOW_CHARGING_ANIMATION:
