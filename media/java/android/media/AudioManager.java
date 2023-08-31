0a3c03456c14a7ea9c18224f39eb4c90a22be7b7
Shunkai Yao <yaoshunkai@google.com>
2023-08-31 22:36:28 +0000

    Avoid Object.wait(0)
    From java doc: If timeout is zero, however, then real time is not
    taken into consideration and the thread simply waits until notified.
    
    Bug: 298456150
    Test: m
    Change-Id: Id5cdf6a52a97fddb24d2fcf2dcaa6e7385eb7c46

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -4594,7 +4594,9 @@ public class AudioManager {
             synchronized (this) {
                 while (!mQuit) {
                     final long timeToWait = timeOutTime - java.lang.System.currentTimeMillis();
-                    if (timeToWait < 0) { break; }
+                    if (timeToWait <= 0) {
+                        break;
+                    }
                     this.wait(timeToWait);
                 }
             }
