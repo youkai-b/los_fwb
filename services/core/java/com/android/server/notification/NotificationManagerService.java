cf75e901c5a3ce35ce6f710aa40d24243bdf54e0
Iavor-Valentin Iftime <valiiftime@google.com>
2024-12-11 02:37:21 -0800

    Check sound Uri permission when creating a notification channel
     Verify that the calling app has the right permissions to read/grant the sound Uri
     for a channel it creates.
     Previously, Uri permissions would only be checked by NotificationRecord when a notification was posted.
    
    Flag: EXEMPT security fix
    
    Test: atest PreferencesHelperTest
    Test: atest NotificationManagerServiceTest
    Bug: 337775777
    
    (cherry picked from commit 94fc92cf20b897f016c3a9718edf40818de4a11b)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:665e1aee2dbad19d436a08d753b4d5cf733abeb5)
    Merged-In: If5a6c80e3bd665a9e67655a230e43d704422082b
    Change-Id: If5a6c80e3bd665a9e67655a230e43d704422082b

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2336,6 +2336,7 @@ public class NotificationManagerService extends SystemService {
                 mPermissionHelper,
                 mNotificationChannelLogger,
                 mAppOps,
+                mUgmInternal,
                 new SysUiStatsEvent.BuilderFactory(),
                 mShowReviewPermissionsNotification);
         mRankingHelper = new RankingHelper(getContext(),
@@ -5809,13 +5810,7 @@ public class NotificationManagerService extends SystemService {
             final Uri originalSoundUri =
                     (originalChannel != null) ? originalChannel.getSound() : null;
             if (soundUri != null && !Objects.equals(originalSoundUri, soundUri)) {
-                Binder.withCleanCallingIdentity(() -> {
-                    mUgmInternal.checkGrantUriPermission(sourceUid, null,
-                            ContentProvider.getUriWithoutUserId(soundUri),
-                            Intent.FLAG_GRANT_READ_URI_PERMISSION,
-                            ContentProvider.getUserIdFromUri(soundUri,
-                            UserHandle.getUserId(sourceUid)));
-                });
+                PermissionHelper.grantUriPermission(mUgmInternal, soundUri, sourceUid);
             }
         }
 
